77f42c6955b04b5040de27481826b0e3ccc28f5d
Riccardo Tedeschi <riccardo.tedeschi6@unibo.it>
2025-03-11 10:11:59 +0100

    portings/riscvbarelib: add cjpeg-rose7-preset workload

---
+++ portings/riscvbarelib/workloads/cjpeg-rose7-preset/cjpeg-rose7-preset.mk
@@ -0,0 +1,14 @@
+OBJS += $(COREMARK_HOME)/workloads/cjpeg-rose7-preset/cjpeg-rose7-preset.o
+OBJS += $(COREMARK_HOME)/benchmarks/consumer_v2/cjpeg/data/Rose256_bmp.o
+OBJS += $(patsubst %.c,%.o,$(wildcard $(COREMARK_HOME)/benchmarks/consumer_v2/cjpeg/*.c))
+
+EXTRA_INCLUDES += -I$(COREMARK_HOME)/benchmarks/consumer_v2/cjpeg/data
+
+ifeq ($(XLEN),64)
+	CFLAGS += -DUSE_FP64
+else
+	CFLAGS += -DUSE_FP32
+endif
+
+CFLAGS += -DSELECT_PRESET_ID=1
+CFLAGS += -DUSE_PRESET
