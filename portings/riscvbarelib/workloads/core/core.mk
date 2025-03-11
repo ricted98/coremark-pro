3e145d6a0faa3ab26b4660d8ed7095c038fb5e2f
Riccardo Tedeschi <riccardo.tedeschi6@unibo.it>
2025-03-11 10:10:53 +0100

    portings/riscvbarelib: ensure FP usage aligns with XLEN

--- portings/riscvbarelib/workloads/core/core.mk
+++ portings/riscvbarelib/workloads/core/core.mk
@@ -1,4 +1,8 @@
 OBJS += $(COREMARK_HOME)/workloads/core/core.o
 OBJS += $(patsubst %.c,%.o,$(wildcard $(COREMARK_HOME)/benchmarks/core/core_*.c))
 
-CFLAGS += -DUSE_FP64
+ifeq ($(XLEN),64)
+	CFLAGS += -DUSE_FP64
+else
+	CFLAGS += -DUSE_FP32
+endif
