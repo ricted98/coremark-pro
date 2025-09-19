53967f78a664979e4e101041021c88276638122e
Riccardo Tedeschi <riccardo.tedeschi6@unibo.it>
2025-09-19 18:22:04 +0200

    portings/riscvbarelib: add nnet_test workload

---
+++ portings/riscvbarelib/workloads/nnet_test/nnet_test.mk
@@ -0,0 +1,11 @@
+OBJS += $(COREMARK_HOME)/workloads/nnet_test/nnet_test.o
+OBJS += $(COREMARK_HOME)/benchmarks/fp/nnet/nnet.o
+OBJS += $(patsubst %.c,%.o,$(wildcard $(COREMARK_HOME)/benchmarks/fp/nnet/ref/*.c))
+
+EXTRA_INCLUDES += -I$(COREMARK_HOME)/benchmarks/fp/nnet
+
+CFLAGS += -DUSE_MATH_H=1
+CFLAGS += -DUSE_FP64=1
+CFLAGS += -DNO_ALIGNED_ALLOC=1
+
+EXTRA_LIBS += -lm
